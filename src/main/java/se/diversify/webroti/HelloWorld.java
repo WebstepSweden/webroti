22683d365561f0b2a21343dd1ed88e4c2330c4b6
Daniel Valfridsson <daniel@valfridsson.net>
2013-02-07 12:22:29 +0100

    first commit

---
+++ src/main/java/se/diversify/webroti/HelloWorld.java
@@ -0,0 +1,8 @@
+package se.diversify.webroti;
+
+/**
+ * @author Daniel Valfridsson (valda)
+ * @version 1.0
+ */
+public class HelloWorld {
+}
---
+++ src/test/java/se/diversify/webroti/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package se.diversify.webroti;
+
+/**
+ * @author Daniel Valfridsson (valda)
+ * @version 1.0
+ */
+public class HelloWorldTest {
+    @org.junit.Test
+    public void testGetName() throws Exception {
+
+    }
+}
