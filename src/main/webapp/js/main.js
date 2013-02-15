86b1ad7cf96f4b14e9b9f9baf7724b39882cb534
Björn Ekengren <bjorn.ekengren@gmail.com>
2013-02-15 11:12:55 +0100

    Basal functionalitet

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,8 +1,28 @@
 (function($){
+    var CONFIRMATION_DELAY = 3000; //ms
+
+
     var meetingId = window.location.hash;
     $('.meeting-id').html(meetingId);
 
     var voteUrl = 'vote.html' + meetingId;
     $('.vote-link').prop('href', voteUrl);
     $('.vote-url').html(voteUrl);
+
+    $('.vote-value').text($('input[type="range"]').get(0).value);
+
+    $('input[type="range"]').on('change', function(e){
+        $('.vote-value').text(this.value);
+    });
+
+    $('.vote-button').on('click', function(e) {
+        e.preventDefault();
+
+        $('.confirmation').modal();
+
+        setTimeout(function(){
+            $('.confirmation').modal('hide');
+        }, CONFIRMATION_DELAY);
+
+    });
 })(jQuery);
