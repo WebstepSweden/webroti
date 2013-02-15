53c9c07cee91f1c50666e59107c602dfe85a766c
Daniel Valfridsson <daniel@valfridsson.net>
2013-02-15 12:39:00 +0100

    get och create Meeting

--- src/main/java/se/diversify/webroti/rest/MeetingResource.java
+++ src/main/java/se/diversify/webroti/rest/MeetingResource.java
@@ -7,10 +7,7 @@ import se.diversify.webroti.data.Vote;
 import se.diversify.webroti.rest.vo.MeetingVO;
 import se.diversify.webroti.rest.vo.VoteVO;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -25,15 +22,29 @@ public class MeetingResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public String getMeeting(@QueryParam("id") String id) {
+        String valueToReturn = "";
+        try {
         Meeting meeting = Repository.getMeeting(id);
+        if(meeting != null) {
+            MeetingVO.Builder builder = MeetingVO.getBuilder().id(meeting.getId());
+            for(Vote vote : meeting.getVotes()) {
+                builder.add(new VoteVO(vote.getValue()));
+            }
 
-        MeetingVO.Builder builder = MeetingVO.getBuilder().id(meeting.getId());
-        for(Vote vote : meeting.getVotes()) {
-            builder.add(new VoteVO(vote.getValue()));
+            valueToReturn = new Gson().toJson(builder.build());
         }
-
-        return new Gson().toJson(builder.build());
+        }catch(IllegalArgumentException e){
+            valueToReturn = new Gson().toJson(e.getMessage());
+        }
+       return valueToReturn;
     }
 
-
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/create")
+    public String createMeeting() {
+        Meeting meeting = Repository.createMeeting();
+        return new Gson().toJson(MeetingVO.getBuilder().id(meeting.getId()).build());
+    }
 }
