9b423a5137a57c4a6fd77ba08bd3ccbdeb076998
Björn Ekengren <bjorn.ekengren@gmail.com>
2013-02-15 10:07:47 +0100

    First demo version

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,1 +1,8 @@
+(function($){
+    var meetingId = window.location.hash;
+    $('.meeting-id').html(meetingId);
 
+    var voteUrl = 'vote.html' + meetingId;
+    $('.vote-link').prop('href', voteUrl);
+    $('.vote-url').html(voteUrl);
+})(jQuery);
